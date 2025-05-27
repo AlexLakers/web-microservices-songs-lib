91d634b4a641527cc5e6e1331f1dc1c9acc3edd1
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-05-27 22:08:27 +0300

    1.Added module frontend-client. 2.Developed all the necessary html pages using tymeleaf. 3.This app as a Client-MVC (OAUTH2 client). It get access token from keycloak after successful authentication using authorization code flow.

---
+++ frontend-client/src/main/java/com/alex/web/microservices/songs/lib/frontend/FrontendClientApplicationRunner.java
@@ -0,0 +1,11 @@
+package com.alex.web.microservices.songs.lib.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendClientApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendClientApplicationRunner.class, args);
+    }
+}
